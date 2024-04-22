62314f5f27c94a58bcfcd608c49a6da829ecb29e
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-04-21 21:04:13 -0600

    dang

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,10 +4,10 @@ import Head from '../elements/Head';
 function notFound() {
     return (
         <>
-        <Head title="404 - Page Not Found" />
-        <h1>404 - Page Not Found</h1>
-        <p>The page you are looking for might have been removed or is temporarily unavailable.</p>
-        <p>Go back to <a href="/">Homepage</a>.</p>
+            <Head title="404 - Page Not Found" />
+            <h1>404 - Page Not Found</h1>
+            <p>The page you are looking for might have been removed or is temporarily unavailable.</p>
+            <p>Go back to <a href="/">Homepage</a>.</p>
         </>
     );
 }
--- src/pages/community.js
+++ src/pages/community.js
@@ -3,7 +3,6 @@ import React from 'react';
 function Community() {
     return (
         <div>
-            {/* Your community page content goes here */}
         </div>
     );
 }

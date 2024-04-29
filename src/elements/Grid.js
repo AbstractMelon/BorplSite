657dace2241cb50936114b5c63ac23c576b662a0
0belous <47227283+0belous@users.noreply.github.com>
2024-04-29 18:04:43 +0100

    css tweaks and removed mod descriptions

--- src/elements/Grid.js
+++ src/elements/Grid.js
@@ -14,11 +14,6 @@ const Grid = ({ source }) => {
         fetchMods();
     }, []);
 
-    function aboutPopup() {
-        let popup = document.querySelector('.aboutPopup');
-        popup.classList.toggle('active');
-    }
-
     return (
         <div>
             <input
@@ -30,18 +25,13 @@ const Grid = ({ source }) => {
             <div className='gallery'>
                 {[
                     ...mods.filter((mod) => mod.name.toLowerCase().includes(searchTerm.toLowerCase())).map((mod) => (
-                        <div key={mod.name} className='mod'>
-                            <img className="modImg" src={`${mod.img}`} alt={mod.name} onClick={() => window.open(mod.github, '_blank') || './503.html'}/>
+                        <div key={mod.name} onClick={() => window.open(mod.github, '_blank') || './503.html'} className='mod'>
+                            <img className="modImg" src={`${mod.img}`} alt={mod.name}/>
                             <p className="modInfo modName">{mod.name}</p>
                             <p className="modInfo modAuthor">{mod.author || 'Mod author not found.'}</p>
-                            <div className="buttonContainer">
-                                <button className="btn About" onClick={aboutPopup}>About</button>
-                                <button className="btn Download" onClick={() => window.open(mod.github, '_blank') || './503.html'}>Download</button>
-                            </div>
-                            <div className="aboutPopup" onClick={aboutPopup}><div className='popupText'>{mod.description || 'Error 503: Text currently not available.'}</div></div>
                         </div>
                     )),
-                    ...Array(Math.max(0, 3 - mods.filter((mod) => mod.name.toLowerCase().includes(searchTerm.toLowerCase())).length)).fill(<div className='mod invisible'></div>)
+                    ...Array(Math.max(0, 4 - mods.filter((mod) => mod.name.toLowerCase().includes(searchTerm.toLowerCase())).length)).fill(<div className='mod invisible'></div>)
                 ]}
             </div>
         </div>
